d18ec4d53885da2f7001ef2d9a3208686b396450
Karina <32144659+karinaserheieva@users.noreply.github.com>
2023-01-29 17:07:34 +0100

    Add files via upload

--- js.js
+++ js.js
@@ -4,13 +4,16 @@ var myCurrentVideo=document.getElementById("myCurrentVideo");
 
 function stopVideo(){
     videoPlayer.style.display="none";
+    myCurrentVideo.pause();
+
 }
 
 function playVideo(file){
 
-    var video = document.getElementById('myCurrentVideo');
+   var video = document.getElementById("myCurrentVideo");
     video.src = file;
     
+    video.play();
 
     //myCurrentVideo.scr=file;
     videoPlayer.style.display="block";
